3778693bad4ca83b04d6bcdb21bbefadf4db7703
User260901 <alexmofinadze0@gmail.com>
2026-06-02 06:37:38 +0400

    feat: configured prisma client and integrate it into project, created prisma schema and all db models

--- backend/src/lib/prisma.ts
+++ backend/src/lib/prisma.ts
@@ -1,7 +1,14 @@
-import "dotenv/config";
+import { PrismaClient } from "../../generated/prisma/client";
 import { PrismaPg } from "@prisma/adapter-pg";
-import { PrismaClient } from "../generated/prisma/client";
-const connectionString = `${process.env.DATABASE_URL}`;
-const adapter = new PrismaPg({ connectionString });
-const prisma = new PrismaClient({ adapter });
-export { prisma };
+import { env } from "../../config/env.js";
+import pg from "pg";
+
+const pool = new pg.Pool({
+  connectionString: env.DATABASE_URL,
+});
+const adapter = new PrismaPg(pool);
+
+export const prisma = new PrismaClient({
+  adapter,
+  log: env.NODE_ENV === "development" ? ["query", "error", "warn"] : ["error"],
+});
