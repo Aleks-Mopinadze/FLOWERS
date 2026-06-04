5b994da5f09d2bf31a9345f7d852257da1a215fd
User260901 <alexmofinadze0@gmail.com>
2026-06-04 05:55:34 +0400

    feat added main routes

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,6 +1,15 @@
 import express from "express";
 import { env } from "../config/env";
+import productsRouter from "./routes/product.routes";
+import ordersRouter from "./routes/order.routes";
+import authRouter from "./routes/auth.routes";
+import usersRouter from "./routes/user.route";
 const app = express();
+
+app.use("/api/v1/auth", authRouter);
+app.use("/api/v1/products", productsRouter);
+app.use("/api/v1/orders", ordersRouter);
+app.use("/api/v1/users", usersRouter);
 
 app.get("/", (req, res) => {
   res.send("hello");
---
+++ backend/src/routes/auth.routes.ts
@@ -0,0 +1,6 @@
+import { Router } from "express";
+import { de } from "zod/locales";
+
+const authRouter = Router();
+
+export default authRouter;
